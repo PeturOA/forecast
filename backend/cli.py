8b5b26e5568b4299649ea5be264994004b64f536
PeturOA <peturolafura@gmail.com>
2025-01-24 01:55:23 +0000

    Start developing interactive interface.

---
+++ backend/cli.py
@@ -0,0 +1,14 @@
+def main():
+    """Prompt user for input and display the desired data.
+    
+    Repeat the process as long as the user's curiosity prevails.
+    """
+    # bid_welcome()
+
+    # report_forecast()
+    # while keep_going():
+    #     report_forecast()
+
+
+if __name__ == "__main__":
+    main()
