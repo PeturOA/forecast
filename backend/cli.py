f9e4049ab72940b6a3725f738672dbd80f090dae
PeturOA <peturolafura@gmail.com>
2025-01-24 02:00:54 +0000

    Create stubs for main loop.

--- backend/cli.py
+++ backend/cli.py
@@ -3,11 +3,27 @@ def main():
     
     Repeat the process as long as the user's curiosity prevails.
     """
-    # bid_welcome()
+    bid_welcome()
 
-    # report_forecast()
-    # while keep_going():
-    #     report_forecast()
+    report_forecast()
+    while keep_going():
+        report_forecast()
+
+
+def bid_welcome() -> None:
+    """Greet user."""
+    raise NotImplementedError
+
+
+def report_forecast() -> None:
+    """Ask user for input and display the forecast data."""
+    raise NotImplementedError
+
+
+def keep_going() -> bool:
+    """Ask user if they want to continue."""
+    raise NotImplementedError
+
 
 
 if __name__ == "__main__":
